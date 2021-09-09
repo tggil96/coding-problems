2cbd931303c3ecb44836c840fd1b1f24ae4a480d
Talha Gillani <talhagilani96@gmail.com>
2021-09-09 16:41:20 +0500

    two sum naive solution

---
+++ two-sum/naive-solution.js
@@ -0,0 +1,29 @@
+/**
+ * time complexity ==> O(n^2)
+ * space complexity ==> O(1)
+ * @param {number[]} arr
+ * @param {number} sum
+ */
+const twoSum = (arr, sum) => {
+  if (!Array.isArray(arr)) {
+    return -1;
+  } else if (arr.length === 0) {
+    return -1;
+  }
+  // loop through each index
+  for (let i = 0; i < arr.length; ++i) {
+    // loop through next consecutive index
+    for (let j = i + 1; j < arr.length; ++j) {
+      if (arr[i] + arr[j] === sum) {
+        return [i, j];
+      }
+    }
+  }
+  return -1;
+};
+
+const arr = [5,2,3,7,9];
+const sum = 14;
+
+console.log(twoSum(arr, 14));
+console.log(twoSum(arr, 0));
